d6eca0ee353cf1a3684d255734da6f00bb15f548
GaboProgramers <gutierrezyosguti@gmail.com>
2023-01-20 22:07:30 -0500

    Fruit V.0.2

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './footer.css'
+import { SiWhatsapp } from 'react-icons/si'
+import { FiInstagram } from 'react-icons/fi'
+import { TbBrandMeta } from 'react-icons/tb'
 
 const Footer = () => {
     return (
         <footer className='footer'>
-            <h1>Hola Mundo</h1>
+            <h2>Fruit's</h2>
+            <div className='link__socials'>
+                <a href="#"><SiWhatsapp className='icon-social' /></a>
+                <a href="#"><FiInstagram className='icon-social' /></a>
+                <a href="#"><TbBrandMeta className='icon-social' /></a>
+            </div>
+            <div className="copy">
+                <span>&copy; Todos Los Derechos Recervados <br /><a href="#">Paper's Company</a> - 2023</span>
+            </div>
         </footer>
     )
 }
